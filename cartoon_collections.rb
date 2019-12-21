19fcdf0d29baf1002a53bfc0ba03dbc557916b2a
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 22:47:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,12 @@
 def roll_call_dwarves(array)
-  # Your code here
   array.each_with_index{|item, index| puts "#{index + 1}. #{item.capitalize}"}
 end
 
 def summon_captain_planet(calls_array)
-  # Your code here
   calls_array.map{|i| "#{i.capitalize}!"}
 end
 
 def long_planeteer_calls(calls_array)
-  # Your code here
   calls_array.any?{|word| word.length > 4}
 end
 
