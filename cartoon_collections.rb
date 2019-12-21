11f89c077999c5299360e81ea984688bd2a5b439
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 22:48:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(calls_array)
   calls_array.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(# code an argument here)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
