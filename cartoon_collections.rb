e1ed6d77d133d9e097a64c024ca542d1d4270c16
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 22:46:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(calls_array)
   # Your code here
-  calls_array.any?{|word| word.length
+  calls_array.any?{|word| word.length > 4
 end
 
 def find_the_cheese# code an argument here
